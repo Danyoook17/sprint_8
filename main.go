146e57cdaa19beda7b5e51cf8fb0b78686dff363
Даниил Левый <danyaplekh@yandex.ru>
2025-09-13 20:30:13 +0300

    100 end

--- main.go
+++ main.go
@@ -97,15 +97,15 @@ func (s ParcelService) Delete(number int) error {
 }
 
 func main() {
-db, err := sql.Open("sqlite", "tracker.db")
-if err != nil {
-	fmt.Println("Ошибка при подключении к БД:", err)
-	return
-}
-defer db.Close()
+	db, err := sql.Open("sqlite", "tracker.db")
+	if err != nil {
+		fmt.Println("Ошибка при подключении к БД:", err)
+		return
+	}
+	defer db.Close()
 
-store := NewParcelStore(db)
-service := NewParcelService(store)
+	store := NewParcelStore(db)
+	service := NewParcelService(store)
 
 	// регистрация посылки
 	client := 1
